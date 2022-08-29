2c3f0d052197eecfcb47fad1f134c451910af4d7
Eric <ettrudell17@gmail.com>
2022-08-28 19:16:10 -0600

    second commit

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -8,7 +8,7 @@ class User:
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
-        self.occupation = data['email']
+        self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
     # Now we use class methods to query our database
@@ -23,10 +23,11 @@ class User:
         # Iterate over the db results and create instances of friends with cls.
         for user in results:
             users.append( cls(user) )
+        # print(users)
         return users
     # class method to save our friend to the database
     @classmethod
     def save(cls, data ):
-        query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(first_name)s , %(last_name)s , %(email)s , NOW() , NOW() );"
+        query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(email)s , NOW() , NOW() );"
         # data is a dictionary that will be passed into the save method from server.py
         return connectToMySQL('users_CR').query_db( query, data )
--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 # STEP THREE
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
 # import the class from model_one.py
-from flask_app.models.model_one import User
+from flask_app.models.user import User
 
 app = Flask(__name__)
+app.secret_key='0bns834w5jkbnsdv9f8yw4'
 
 @app.route("/")
 def index():
@@ -15,21 +16,22 @@ def index():
 
 @app.route('/create_user', methods=["POST"])
 def create_user():
-    
     # First we make a data dictionary from our request.form coming from our template.
     # The keys in data need to line up exactly with the variables in our query string.
     data = {
-        "firstname": request.form["first_name"],
-        "last_name" : request.form["last_name"],
-        "email" : request.form["email"]
-    }
+        "fname": request.form["first_name"],
+        "lname" : request.form["last_name"],
+        "email" : request.form["email"]}
     # We pass the data dictionary into the save method from the Friend class.
     User.save(data)
     # Redirect after saving to the database.
-    return redirect('/process')
+    return redirect('/display')
+@app.route("/display")
+def display():
+    users = User.get_all()
+    print("Displaying USERS!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print(users)
+    return render_template("read_all.html", users=users)
 
-@app.route('/process')
-def process():
-    return render_template('read_all.html')
 if __name__ == "__main__":
     app.run(debug=True)
